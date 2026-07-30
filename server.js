b7d3a1c470afc2ad4db789abbbcf193b2797e705
majidH <majid.hadadian@gmail.com>
2026-07-30 15:53:47 +0330

    git commit -m "feat: implement CRUD operations with Mongoose (get, create, update tours)"

--- server.js
+++ server.js
@@ -17,38 +17,7 @@ mongoose
     console.error('DB connection error:', err);
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
 
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
-  price: 497,
-});
-
-testTour
-  .save()
-  .then(() => {
-    console.log('Test tour saved successfully');
-  })
-  .catch((err) => {
-    console.error('Error saving test tour:', err);
-  });
 
 app.listen(8000, () => {
   console.log('App running on port 8000...');
